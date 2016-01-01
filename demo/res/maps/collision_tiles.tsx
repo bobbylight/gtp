9328dbc7447c55ac4b3541eff9083a09116a569b
bobbylight <robert.e.futrell@gmail.com>
2016-01-01 16:29:54 -0500

    Tidying up gh-pages

--- demo/res/maps/collision_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<tileset name="collision" tilewidth="16" tileheight="16" spacing="1">
-   <image source="../collision.png" width="50" height="50"/>
-</tileset>
