27d09e4f02fa2b60c80d5b3f75db293a9b57a06a
bobbylight <robert.e.futrell@gmail.com>
2017-01-10 22:54:50 -0500

    Adding unit test for Sound class

---
+++ src/gtp/Sound.spec.ts
@@ -0,0 +1,32 @@
+import {Sound} from '../index';
+
+describe('Sound', () => {
+
+	it('constructor happy path, no loop start', () => {
+		const sound: Sound = new Sound('id', 'buffer');
+		expect(sound.getId()).toBe('id');
+		expect(sound.getBuffer()).toBe('buffer');
+		expect(sound.getLoopStart()).toBe(0);
+	});
+
+	it('constructor happy path, with loop start', () => {
+		const sound: Sound = new Sound('id', 'buffer', 42);
+		expect(sound.getId()).toBe('id');
+		expect(sound.getBuffer()).toBe('buffer');
+		expect(sound.getLoopStart()).toBe(42);
+	});
+
+	it('get/set loopsByDefaultIfMusic', () => {
+		const sound: Sound = new Sound('id', 'buffer', 42);
+		expect(sound.getLoopsByDefaultIfMusic()).toBeTruthy();
+		sound.setLoopsByDefaultIfMusic(false);
+		expect(sound.getLoopsByDefaultIfMusic()).toBeFalsy();
+	});
+
+	it('get/set loopStart', () => {
+		const sound: Sound = new Sound('id', 'buffer', 42);
+		expect(sound.getLoopStart()).toBe(42);
+		sound.setLoopStart(17);
+		expect(sound.getLoopStart()).toBe(17);
+	});
+});
