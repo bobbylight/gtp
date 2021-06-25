0556f16532fe8a5373981a418e73671d8913513d
bobbylight <robert.e.futrell@gmail.com>
2021-06-24 23:30:55 -0400

    Bumping version of typedoc

---
+++ typedoc-class-name-fixing-plugin.js
@@ -0,0 +1,13 @@
+// Ugh, see:
+// https://github.com/TypeStrong/typedoc/issues/1521
+
+// TypeDoc 0.20.x, CC0
+const { Converter } = require("typedoc");
+
+exports.load = function({ application }) {
+    application.converter.on(Converter.EVENT_CREATE_DECLARATION, (_context, reflection, node) => {
+        if (!node || !node.name || reflection.name !== "default") return;
+
+        reflection.name = node.name.getText();
+    });
+};
