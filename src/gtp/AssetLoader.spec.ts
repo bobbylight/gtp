088bca63067f400d0527f830ebd250359f9d9bea
bobbylight <robert.e.futrell@gmail.com>
2022-01-03 22:36:27 -0500

    Yet more AssetLoader tests

--- src/gtp/AssetLoader.spec.ts
+++ src/gtp/AssetLoader.spec.ts
@@ -14,12 +14,25 @@ describe('AssetLoader', () => {
 		decodeAudioData: (audioData: ArrayBuffer, successCallback: DecodeSuccessCallback) => {
 			successCallback({} as AudioBuffer);
 		},
+		createBufferSource(): AudioBufferSourceNode {
+			return {
+				connect: jest.fn(),
+				start: jest.fn(),
+				stop: jest.fn(),
+				disconnect: jest.fn(),
+				context: {
+					currentTime: 0,
+				},
+			} as unknown as AudioBufferSourceNode;
+		},
 		createGain: () => {
 			return {
 				gain: {
-					setValueAtTime: () => {}
+					setValueAtTime: () => {},
+					linearRampToValueAtTime: () => {},
 				},
-				connect: () => {}
+				connect: () => {},
+				disconnect: () => {},
 			};
 		}
 	}));
@@ -40,218 +53,285 @@ describe('AssetLoader', () => {
 		jest.restoreAllMocks();
 	});
 
-	it('constructor, happy path', () => {
-		new AssetLoader(1, new AudioSystem());
-	});
+	// Loop through no assetRoot value and an assetRoot value
+	describe.each`
+		assetRoot
+		${undefined}
+		${'foo'}
+	`('when assetRoot is $assetRoot', ({ assetRoot }) => {
+		it('constructor, happy path', () => {
+			new AssetLoader(1, new AudioSystem());
+		});
 
-	it('addJson() loads the data', () => {
+		it('addJson() loads the data', () => {
 
-		window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		// Initially nothing queued up, so we're "done loading"
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem(), assetRoot);
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		expect(assetLoader.get('testJson')).toBeUndefined();
+			expect(assetLoader.get('testJson')).toBeUndefined();
 
-		// Add something to load, now we're waiting
-		assetLoader.addJson('testJson', '/fake/url.json');
-		expect(assetLoader.isDoneLoading()).toBeFalsy();
+			// Add something to load, now we're waiting
+			assetLoader.addJson('testJson', '/fake/url.json');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		// Mock a response, which triggers AssetLoader to receive the response as well
-		xhrMock.readyState = XMLHttpRequest.DONE;
-		xhrMock.responseText = '{"test": "json"}';
-		xhrMock.onreadystatechange();
+			// Mock a response, which triggers AssetLoader to receive the response as well
+			xhrMock.readyState = XMLHttpRequest.DONE;
+			xhrMock.responseText = '{"test": "json"}';
+			xhrMock.onreadystatechange();
 
-		// Now that we've received the response, check that we're "done loading" again
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
-		expect(assetLoader.get('testJson')).toEqual({ test: 'json' });
-	});
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testJson')).toEqual({ test: 'json' });
+		});
 
-	it('addCanvas() loads the data', async () => {
+		// Asking to load two things with the same ID is likely a bug in the app, but still
+		// verify the behavior here
+		it('addJson() does nothing if data with the given ID is currently being loaded', () => {
 
-		window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		// Initially nothing queued up, so we're "done loading"
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem(), assetRoot);
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		expect(assetLoader.get('testCanvas')).toBeUndefined();
+			expect(assetLoader.get('testJson')).toBeUndefined();
 
-		// Add something to load, now we're waiting
-		assetLoader.addCanvas('testCanvas',
-			'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
-		expect(assetLoader.isDoneLoading()).toBeFalsy();
+			// Add something to load, now we're waiting
+			assetLoader.addJson('testJson', '/fake/url.json');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		await new Promise(r => setTimeout(r, 50));
+			// Try to load the same item again - this should change nothing
+			assetLoader.addJson('testJson', '/fake/url.json');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		// Now that we've received the response, check that we're "done loading" again
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
-		expect(assetLoader.get('testCanvas')).toBeDefined();
-	});
+			// Mock a response, which triggers AssetLoader to receive the response as well
+			xhrMock.readyState = XMLHttpRequest.DONE;
+			xhrMock.responseText = '{"test": "json"}';
+			xhrMock.onreadystatechange();
 
-	it('addImage() loads the data', async () => {
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testJson')).toEqual({ test: 'json' });
+		});
 
-		window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
+		// Asking to load two things with the same ID is likely a bug in the app, but still
+		// verify the behavior here
+		it('addJson() does nothing if data with the given ID has already been loaded', () => {
 
-		// Initially nothing queued up, so we're "done loading"
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		expect(assetLoader.get('testImage')).toBeUndefined();
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem(), assetRoot);
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		// Add something to load, now we're waiting
-		assetLoader.addImage('testImage',
-			'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
-		expect(assetLoader.isDoneLoading()).toBeFalsy();
+			expect(assetLoader.get('testJson')).toBeUndefined();
 
-		await new Promise(r => setTimeout(r, 50));
+			// Add something to load, now we're waiting
+			assetLoader.addJson('testJson', '/fake/url.json');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		// Now that we've received the response, check that we're "done loading" again
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
-		expect(assetLoader.get('testImage')).toBeDefined();
-	});
+			// Mock a response, which triggers AssetLoader to receive the response as well
+			xhrMock.readyState = XMLHttpRequest.DONE;
+			xhrMock.responseText = '{"test": "json"}';
+			xhrMock.onreadystatechange();
 
-	it('addImageAtlasContents() loads the data', async () => {
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testJson')).toEqual({ test: 'json' });
 
-		window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
+			// Try to load the same item again - this should change nothing
+			assetLoader.addJson('testJson', '/fake/url.json');
+			expect(assetLoader.isDoneLoading()).toBeTruthy(); // Already loaded 'testJson'
+		});
 
-		// Initially nothing queued up, so we're "done loading"
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
+		it('addCanvas() loads the data', async () => {
 
-		expect(assetLoader.get('testImage')).toBeUndefined();
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		const atlasInfo: ImageAtlasInfo = {
-			prefix: 'testAtlasImages',
-			images: [
-				{
-					id: 'foo',
-					x: 0,
-					y: 0,
-					w: 0,
-					h: 0,
-				},
-			],
-		};
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		// Add something to load, now we're waiting
-		assetLoader.addImageAtlasContents('unused',
-			'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
-			atlasInfo);
-		expect(assetLoader.isDoneLoading()).toBeFalsy();
+			expect(assetLoader.get('testCanvas')).toBeUndefined();
 
-		await new Promise(r => setTimeout(r, 50));
+			// Add something to load, now we're waiting
+			assetLoader.addCanvas('testCanvas',
+				'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		// Now that we've received the response, check that we're "done loading" again
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
-		expect(assetLoader.get('testAtlasImagesfoo')).toBeDefined();
-	});
+			await new Promise(r => setTimeout(r, 50));
 
-	it('addSound() loads sound data if audio is enabled in this browser', () => {
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testCanvas')).toBeDefined();
+		});
 
-		window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
+		it('addImage() loads the data', async () => {
 
-		// Initially nothing queued up, so we're "done loading"
-		const audioSystem: AudioSystem = new AudioSystem();
-		expect(audioSystem.init()).toBeTruthy();
-		const assetLoader: AssetLoader = new AssetLoader(1, audioSystem);
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		expect(assetLoader.get('testSound')).toBeUndefined();
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		// Add something to load, now we're waiting
-		assetLoader.addSound('testSound', '/fake/sound.ogg');
-		expect(assetLoader.isDoneLoading()).toBeFalsy();
+			expect(assetLoader.get('testImage')).toBeUndefined();
 
-		// Mock a response, which triggers AssetLoader to receive the response as well
-		xhrMock.readyState = XMLHttpRequest.DONE;
-		xhrMock.response = new ArrayBuffer(10);
-		xhrMock.onload();
+			// Add something to load, now we're waiting
+			assetLoader.addImage('testImage',
+				'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		// Now that we've received the response, check that we're "done loading" again
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
-		expect(assetLoader.get('testSound')).toBeDefined();
-	});
+			await new Promise(r => setTimeout(r, 50));
 
-	it('addSpriteSheet() loads the data', async () => {
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testImage')).toBeDefined();
+		});
 
-		window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
+		it('addImageAtlasContents() loads the data', async () => {
 
-		// Initially nothing queued up, so we're "done loading"
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		expect(assetLoader.get('testImage')).toBeUndefined();
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		// Add something to load, now we're waiting
-		assetLoader.addSpriteSheet('testSheet',
-			'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
-			1, 1, 0, 0, true);
-		expect(assetLoader.isDoneLoading()).toBeFalsy();
+			expect(assetLoader.get('testImage')).toBeUndefined();
 
-		await new Promise(r => setTimeout(r, 50));
+			const atlasInfo: ImageAtlasInfo = {
+				prefix: 'testAtlasImages',
+				images: [
+					{
+						id: 'foo',
+						x: 0,
+						y: 0,
+						w: 0,
+						h: 0,
+					},
+				],
+			};
 
-		// Now that we've received the response, check that we're "done loading" again
-		expect(assetLoader.isDoneLoading()).toBeTruthy();
-		expect(assetLoader.get('testSheet')).toBeDefined();
-	});
+			// Add something to load, now we're waiting
+			assetLoader.addImageAtlasContents('unused',
+				'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+				atlasInfo);
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-	it('addTmxMap() works with undefined tilesets', () => {
+			await new Promise(r => setTimeout(r, 50));
 
-		const data: TiledMapData = {
-			width: 1,
-			height: 1,
-			version: 1,
-			layers: [],
-			properties: [],
-			orientation: 'not-sure',
-		};
-		const args: TiledMapArgs = {
-			tileWidth: 16,
-			tileHeight: 16,
-			screenWidth: 1,
-			screenHeight: 1,
-		};
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testAtlasImagesfoo')).toBeDefined();
+		});
 
-		const map: TiledMap = new TiledMap(data, args);
+		it('addSound() loads sound data if audio is enabled in this browser', () => {
 
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		assetLoader.addTmxMap(map); // No errors
-	});
+			// Initially nothing queued up, so we're "done loading"
+			const audioSystem: AudioSystem = new AudioSystem();
+			expect(audioSystem.init()).toBeTruthy();
+			const assetLoader: AssetLoader = new AssetLoader(1, audioSystem);
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-	it('addTmxMap() works with empty tilesets', () => {
+			expect(assetLoader.get('testSound')).toBeUndefined();
 
-		const data: TiledMapData = {
-			width: 1,
-			height: 1,
-			version: 1,
-			layers: [],
-			properties: [],
-			orientation: 'not-sure',
-			tilesets: [],
-		};
-		const args: TiledMapArgs = {
-			tileWidth: 16,
-			tileHeight: 16,
-			screenWidth: 1,
-			screenHeight: 1,
-		};
+			// Add something to load, now we're waiting
+			assetLoader.addSound('testSound', '/fake/sound.ogg');
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
 
-		const map: TiledMap = new TiledMap(data, args);
+			// Mock a response, which triggers AssetLoader to receive the response as well
+			xhrMock.readyState = XMLHttpRequest.DONE;
+			xhrMock.response = new ArrayBuffer(10);
+			xhrMock.onload();
 
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testSound')).toBeDefined();
+		});
 
-		assetLoader.addTmxMap(map); // No errors
-	});
+		it('addSpriteSheet() loads the data', async () => {
 
-	it('set() works to manually add a resource', () => {
+			window.XMLHttpRequest = jest.fn().mockImplementation(() => xhrMock) as any;
 
-		const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
-		expect(assetLoader.get('foo')).toBeUndefined();
+			// Initially nothing queued up, so we're "done loading"
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
 
-		assetLoader.set('foo', 14);
-		expect(assetLoader.get('foo')).toEqual(14);
+			expect(assetLoader.get('testImage')).toBeUndefined();
+
+			// Add something to load, now we're waiting
+			assetLoader.addSpriteSheet('testSheet',
+				'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+				1, 1, 0, 0, true);
+			expect(assetLoader.isDoneLoading()).toBeFalsy();
+
+			await new Promise(r => setTimeout(r, 50));
+
+			// Now that we've received the response, check that we're "done loading" again
+			expect(assetLoader.isDoneLoading()).toBeTruthy();
+			expect(assetLoader.get('testSheet')).toBeDefined();
+		});
+
+		it('addTmxMap() works with undefined tilesets', () => {
+
+			const data: TiledMapData = {
+				width: 1,
+				height: 1,
+				version: 1,
+				layers: [],
+				properties: [],
+				orientation: 'not-sure',
+			};
+			const args: TiledMapArgs = {
+				tileWidth: 16,
+				tileHeight: 16,
+				screenWidth: 1,
+				screenHeight: 1,
+			};
+
+			const map: TiledMap = new TiledMap(data, args);
+
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+
+			assetLoader.addTmxMap(map); // No errors
+		});
+
+		it('addTmxMap() works with empty tilesets', () => {
+
+			const data: TiledMapData = {
+				width: 1,
+				height: 1,
+				version: 1,
+				layers: [],
+				properties: [],
+				orientation: 'not-sure',
+				tilesets: [],
+			};
+			const args: TiledMapArgs = {
+				tileWidth: 16,
+				tileHeight: 16,
+				screenWidth: 1,
+				screenHeight: 1,
+			};
+
+			const map: TiledMap = new TiledMap(data, args);
+
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+
+			assetLoader.addTmxMap(map); // No errors
+		});
+
+		it('set() works to manually add a resource', () => {
+
+			const assetLoader: AssetLoader = new AssetLoader(1, new AudioSystem());
+			expect(assetLoader.get('foo')).toBeUndefined();
+
+			assetLoader.set('foo', 14);
+			expect(assetLoader.get('foo')).toEqual(14);
+		});
 	});
 });
