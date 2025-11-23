057f222242c67145d9e5fc4f2c975b359755058a
bobbylight <robert.e.futrell@gmail.com>
2025-11-22 21:25:04 -0500

    fix: Don't log errors when AudioSystem not defined

--- src/gtp/AssetLoader.ts
+++ src/gtp/AssetLoader.ts
@@ -228,7 +228,7 @@ export default class AssetLoader {
 
 			const xhr: XMLHttpRequest = new XMLHttpRequest();
 			xhr.onload = () => {
-				// this.autio.context is definitely defined since audio initialized
+				// this.audio.context is definitely defined since audio initialized
 				// TODO: Clean up this API
 				void this.audio.context!.decodeAudioData(xhr.response as ArrayBuffer, (buffer: AudioBuffer) => {
 					const sound: Sound = new Sound(id, buffer, loopStart || 0);
